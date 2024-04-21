76a9b6f1e61bdc37cbac06b7cc64c3df65754e53
KneerazPandey <nirajpandeyniraj77@gmail.com>
2024-04-21 15:20:55 +0545

    change in dynamic array

--- array/dynamic_array.py
+++ array/dynamic_array.py
@@ -96,4 +96,25 @@ class DynamicArray(object):
         self.array = new_array 
         self.capacity = new_capacity
         
-    
+
+
+
+if __name__ == '__main__':
+    a = DynamicArray()
+
+    a.append(1)
+    a.append(2)
+    a.append(3)
+    a.insert(0, 0)
+    a.insert(1.5, 2)
+    a.insert(4, 5)
+
+    print(a.pop(2))
+    print(a.remove_at(1))
+    print(a.delete(3))
+
+    a[0] = 1
+    a[2] = 3
+
+    print(a)
+    print(f'The size of an array is: {len(a)}')
