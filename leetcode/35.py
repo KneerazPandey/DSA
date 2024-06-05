0a5b1bbfd778da223f832d08accaf2789be357a9
KneerazPandey <nirajpandeyniraj77@gmail.com>
2024-06-05 21:02:41 +0545

    completed leetcode question number 35. Search Insert Position problems

---
+++ leetcode/35.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search_insert(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid 
+            
+            if target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
+    
+    
+nums = [1,3,5,6]
+target = 7
+s = Solution()
+print(s.search_insert(nums=nums, target=target))
